0c8ef0980241e85ac545df96ff64c1af29d3ac2d
Elman Farziyev <elmanfarziyevv@gmail.com>
2026-08-29 14:47:36 -0400

    Update cart tests to use fixtures
    Refactor tests to use fixtures for login and products pages.

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -1,31 +1,27 @@
-import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/LoginPage';
-import { ProductsPage } from '../pages/ProductsPage';
+import { test, expect } from '../fixtures/testFixtures';
 import { users } from '../test-data/users';
 
 test.describe('Shopping Cart', () => {
 
-  test.beforeEach(async ({ page }) => {
-    const loginPage = new LoginPage(page);
-
+  test.beforeEach(async ({ loginPage }) => {
     await loginPage.open();
+
     await loginPage.login(
       users.standard.username,
       users.standard.password
     );
   });
 
-  test('user can add a product to the cart', async ({ page }) => {
-    const productsPage = new ProductsPage(page);
-
+  test('user can add a product to the cart', async ({ productsPage }) => {
     await productsPage.addBackpackToCart();
 
     await expect(productsPage.cartBadge).toHaveText('1');
   });
 
-  test('added product is displayed in the cart', async ({ page }) => {
-    const productsPage = new ProductsPage(page);
-
+  test('added product is displayed in the cart', async ({
+    page,
+    productsPage
+  }) => {
     await productsPage.addBackpackToCart();
     await productsPage.openCart();
 
